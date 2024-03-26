c346382d92773546794cd8d9fd2f8bed13e66bc6
Johan Selin <johanselin99@gmail.com>
2024-03-26 12:27:16 +0100

    Clean up imports, formatting, and error messages

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -1,54 +1,47 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-} -- Turn off unused import warning off in stub
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PatternSynonyms #-}
 
 module TypeChecker where
 
-import Control.Applicative
-import Control.Monad
-import Control.Monad.Except
-import Control.Monad.Reader
-import Control.Monad.State
+import           Control.Monad
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import           Control.Monad.State
 
-import Data.Foldable (toList)
-import Data.Functor
-import Data.List.NonEmpty (pattern (:|), (<|))
+import           Data.Foldable (toList)
+import           Data.List.NonEmpty (pattern (:|))
 import qualified Data.List.NonEmpty as List1
-import Data.Maybe
-import Data.List (groupBy, sort)
-import qualified Data.Set as Set
-
-import Data.Map (Map)
+import           Data.Maybe
+import           Data.List (groupBy, sort)
+import           Data.Map (Map)
 import qualified Data.Map as Map
 
-import Javalette.Abs
-import qualified Javalette.Abs as Abs
-import Javalette.Print (printTree)
-
-import FunType
+import           Javalette.Abs
+import           Javalette.Print (printTree, Print)
 import qualified Annotated as A
-import Control.Monad.Trans.Except (throwE)
+import           FunType
 
 type AnnotatedProgram = A.Prog
 type TypeError = String
-
-type List1 = List1.NonEmpty
-
+type Check = ReaderT Sig (StateT St (Except TypeError))
 type Sig = Map Ident FunType
+type Cxt   = List1.NonEmpty Block
+type Block = Map Ident Type
 
 data St = St
     { stCxt :: Cxt
     , stRet :: Type
     }
 
-type Cxt   = List1 Block
-type Block = Map Ident Type
+pt :: forall a. Print a => a -> String
+pt = printTree
+
+fmt :: [String] -> String
+fmt = concatMap (++ " ")
 
 initSt :: Type -> St
 initSt = St $ Map.empty :| []
-
-type Check = ReaderT Sig (StateT St (Except TypeError))
 
 builtin :: [(Ident, FunType)]
 builtin =
@@ -69,12 +62,12 @@ typecheck prg@(Program defs) = do
     runExcept (evalStateT (runReaderT (checkProgram prg) (Map.fromList sig)) st)
     where sourceFuncs = map (\(FnDef t fId args _) -> (fId, FunType t (map (\(Argument t _) -> t) args)))
           assertHasMain s = unless ((Ident "main", FunType Int []) `elem` s) $ throwError "no main function"
-          assertNoStringReturns defs = do 
+          assertNoStringReturns defs = do
             let strRetFuncs = [id | FnDef t id _ _ <- defs, t == Str]
-            unless (null strRetFuncs) $ throwError $ "function may not return type string: " ++ printTree (head strRetFuncs)
+            unless (null strRetFuncs) $ throwError $ fmt ["function may not return type string:", pt (head strRetFuncs)]
           assertNoDuplicateFuns s = do
             let duplicates = filter (\x -> length x > 1) $ groupBy (\t1 t2 -> fst t1 == fst t2) $ sort s
-            unless (null duplicates) $ throwError $ "duplicate function definition: " ++ printTree (fst (head $ head duplicates))
+            unless (null duplicates) $ throwError $ fmt ["duplicate function definition:", pt (fst (head $ head duplicates))]
 
 checkProgram :: Prog -> Check A.Prog
 checkProgram (Program defs) = do
@@ -86,43 +79,42 @@ checkDef :: TopDef -> Check A.TopDef
 checkDef (FnDef t fId args ss) = do
     put $ initSt t
     mapM_ (\(Argument t x) -> newVar x t) args
-    ss' <- checkStms ss
+    ss' <- checkStmts ss
     return $ A.FnDef t fId args (A.Block ss')
 
 checkReturn :: TopDef -> Check ()
 checkReturn (FnDef Void fId _ ss) = return ()
-checkReturn (FnDef t fId _ (Block ss)) = if any checkPath ss then
+checkReturn (FnDef t fId _ (Block ss)) = if any hasReturn ss then
         return ()
     else
-        throwError $ "Not every execution path returns in function: " ++ printTree fId
+        throwError $ fmt ["Not every execution path returns in function:", pt fId]
 
-checkPath :: Stmt -> Bool
-checkPath = \case
+hasReturn :: Stmt -> Bool
+hasReturn = \case
     Ret _ -> True
-    CondElse _ s1 s2 -> checkPath s1 && checkPath s2
-    (BStmt (Block ss)) -> any checkPath ss
+    CondElse _ s1 s2 -> hasReturn s1 && hasReturn s2
+    (BStmt (Block ss)) -> any hasReturn ss
     _ -> False
 
-checkInit :: Type -> [Item] -> Check [A.Item]
-checkInit t = go
-    where go :: [Item] -> Check [A.Item]
-          go [] = return []
-          go (i:is) = do
-              i' <- checkItem i
-              is' <- go is
-              return $ i' : is'
-          checkItem :: Item -> Check A.Item
-          checkItem = \case
-              NoInit id -> do
-                  newVar id t
-                  return $ A.NoInit id
-              Init id e -> do
-                  newVar id t
-                  e' <- checkExp e t
-                  return $ A.Init id e'
+checkItems :: Type -> [Item] -> Check [A.Item]
+checkItems t [] = return []
+checkItems t (i:is) = do
+    i' <- checkItem t i
+    is' <- checkItems t is
+    return $ i' : is'
 
-checkStm :: Stmt -> Check A.Stmt
-checkStm = \case
+checkItem :: Type -> Item -> Check A.Item
+checkItem t = \case
+    NoInit id -> do
+        newVar id t
+        return $ A.NoInit id
+    Init id e -> do
+        newVar id t
+        e' <- checkExpr e t
+        return $ A.Init id e'
+
+checkStmt :: Stmt -> Check A.Stmt
+checkStmt = \case
     Empty -> return A.Empty
 
     SExp e -> do
@@ -130,7 +122,7 @@ checkStm = \case
             EApp _ _ -> do
                 (t, e') <- inferExp e
                 return $ A.SExp t e'
-            _ -> throwError $ "Void expressions are not allowed: " ++ printTree e
+            _ -> throwError $ fmt ["Non-void expressions except for function calls are not allowed:", pt e]
 
     Ass id e -> do
         (t, e') <- inferExp e
@@ -138,29 +130,29 @@ checkStm = \case
         if t == t' then
                 return $ A.Ass t id e'
             else
-                throwError $ "Type mismatch: " ++ printTree e ++ ". Expected " ++ printTree t' ++ ", got " ++ printTree t
+                throwError $ fmt ["Type mismatch:", pt e, ". Expected", pt t', ", got", pt t]
 
     Incr id -> do
         t <- lookupVar id
-        unless (t == Int) $ throwError $ "Type mismatch. Cannot apply '++' to " ++ printTree t
+        unless (t == Int) $ throwError $ fmt ["Type mismatch. Cannot apply '++' to", pt t]
         return $ A.Incr t id
 
     Decr id -> do
         t <- lookupVar id
-        unless (t == Int) $ throwError $ "Type mismatch. Cannot apply '--' to " ++ printTree t
+        unless (t == Int) $ throwError $ fmt ["Type mismatch. Cannot apply '--' to", pt t]
         return $ A.Decr t id
 
     Decl t items -> do
-        items' <- checkInit t items
+        items' <- checkItems t items
         return $ A.Decl t items'
-        
+
     Ret e -> do
         (t, e') <- inferExp e
         t' <- gets stRet
         if t == t' then
                 return $ A.Ret t e'
             else
-                throwError $ "Type mismatch: " ++ printTree e ++ ". Expected " ++ printTree t' ++ ", but function returns " ++ printTree t
+                throwError $ fmt ["Type mismatch:", pt e, ". Expected", pt t', ", but function returns", pt t]
 
     VRet -> do
         t <- gets stRet
@@ -169,36 +161,36 @@ checkStm = \case
 
     Cond e s -> do
         (t, e') <- inferExp e
-        unless (t == Bool) $ throwError $ "Type mismatch: " ++ printTree e ++ ". Expected boolean, got " ++ printTree t
-        s' <- checkStm $ blockify s
+        unless (t == Bool) $ throwError $ fmt ["Type mismatch:", pt e, ". Expected boolean, got", pt t]
+        s' <- checkStmt $ blockify s
         return $ A.Cond e' s'
 
     CondElse e s1 s2 -> do
         (t, e') <- inferExp e
-        unless (t == Bool) $ throwError $ "Type mismatch: " ++ printTree e ++ ". Expected boolean, got " ++ printTree t
-        s1' <- checkStm $ blockify s1
-        s2' <- checkStm $ blockify s2
+        unless (t == Bool) $ throwError $ fmt ["Type mismatch:", pt e, ". Expected boolean, got ", pt t]
+        s1' <- checkStmt $ blockify s1
+        s2' <- checkStmt $ blockify s2
         return $ A.CondElse e' s1' s2'
 
     While e s -> do
         (t, e') <- inferExp e
-        unless (t == Bool) $ throwError $ "Type mismatch: " ++ printTree e ++ ". Expected boolean, got " ++ printTree t
-        s' <- checkStm $ blockify s
+        unless (t == Bool) $ throwError $ fmt ["Type mismatch:", pt e, ". Expected boolean, got", pt t]
+        s' <- checkStmt $ blockify s
         return $ A.While e' s'
 
     BStmt ss -> do
         sret <- gets stRet
         bs <- gets stCxt
         put $ St (Map.empty :| toList bs) sret
-        ss' <- checkStms ss
+        ss' <- checkStmts ss
         put $ St bs sret
         return $ A.BStmt (A.Block ss')
 
     where blockify (BStmt ss) = BStmt ss
           blockify ss = BStmt (Block [ss])
 
-checkStms :: Blk -> Check [A.Stmt]
-checkStms (Block ss) = mapM checkStm ss
+checkStmts :: Blk -> Check [A.Stmt]
+checkStmts (Block ss) = mapM checkStmt ss
 
 inferExp :: Expr -> Check (Type, A.Expr)
 inferExp = \case
@@ -217,13 +209,12 @@ inferExp = \case
     EApp fid es -> do
         sig <- ask
         case Map.lookup fid sig of
-            Nothing -> throwError $ "undefined function: " ++ printTree fid
+            Nothing -> throwError $ fmt ["undefined function:", pt fid]
             Just (FunType t ts) -> do
-                unless (length ts == length es) $ throwError $ "function: " ++ printTree fid ++ " expected " ++ show (length ts) ++ " arguments, got " ++ show (length es)
-                es' <- zipWithM checkExp es ts
+                unless (length ts == length es) $ throwError $ fmt ["function:", pt fid, "expected", show (length ts), "arguments, got", show (length es)]
+                es' <- zipWithM checkExpr es ts
                 return (t, A.EApp fid es')
 
-    -- Set type to void, as the BNF does not specify a type for the string literal
     EString s -> return (Str, A.EString s)
 
     Neg e -> do
@@ -231,11 +222,11 @@ inferExp = \case
         case t of
             Int  -> return (Int, A.Neg e')
             Doub -> return (Doub, A.Neg e')
-            _        -> throwError $ "Type mismatch: " ++ printTree e ++ ". Cannot negate (-) to " ++ printTree t
+            _        -> throwError $ fmt ["Type mismatch:", pt e, ". Cannot apply negate '-' to type", pt t]
 
     Not e -> do
         (t, e') <- inferExp e
-        unless (t == Bool) $ throwError $ "Type mismatch: " ++ printTree e ++ ". Cannot apply '!' to " ++ printTree t
+        unless (t == Bool) $ throwError $ fmt ["Type mismatch:", pt e, ". Cannot apply '!' to type", pt t]
         return (Bool, A.Not e')
 
     EMul e1 op e2 -> do
@@ -244,9 +235,9 @@ inferExp = \case
         case (t1, t2) of
             (Int,  Int)  -> return (Int, A.EMul Int e1' op e2')
             (Doub, Doub) -> do
-                unless (op /= Mod) $ throwError $ "Type mismatch: " ++ printTree e1 ++ " " ++ printTree op ++ " " ++ printTree e2 ++ ". Cannot apply '%' to " ++ printTree t1 ++ " and " ++ printTree t2
+                unless (op /= Mod) $ throwError $ fmt ["Type mismatch:", pt e1, pt op, pt e2, ". Cannot apply '%' to", pt t1, "and", pt t2]
                 return (Doub, A.EMul Doub e1' op e2')
-            _                    -> throwError $ "Type mismatch: " ++ printTree e1 ++ " " ++ printTree op ++ " " ++ printTree e2 ++ ". Cannot apply '" ++ printTree op ++ "' to " ++ printTree t1 ++ " and " ++ printTree t2
+            _                    -> throwError $ fmt ["Type mismatch:", pt e1, pt op, pt e2, ". Cannot apply", pt op, "to", pt t1, "and", pt t2]
 
     EAdd e1 op e2 -> do
         (t1, e1') <- inferExp e1
@@ -254,7 +245,7 @@ inferExp = \case
         case (t1, t2) of
             (Int,  Int)  -> return (Int, A.EAdd Int e1' op e2')
             (Doub, Doub) -> return (Doub, A.EAdd Doub e1' op e2')
-            _                    -> throwError $ "Type mismatch: " ++ printTree e1 ++ " " ++ printTree op ++ " " ++ printTree e2 ++ ". Cannot apply '" ++ printTree op ++ "' to " ++ printTree t1 ++ " and " ++ printTree t2
+            _                    -> throwError $ fmt ["Type mismatch:", pt e1, pt op, pt e2, ". Cannot apply", pt op, "to", pt t1, "and", pt t2]
 
     ERel e1 op e2 -> do
         (t1, e1') <- inferExp e1
@@ -263,41 +254,41 @@ inferExp = \case
             (Bool, Bool) -> return (Bool, A.ERel Bool e1' op e2')
             (Int, Int)   -> return (Bool, A.ERel Int e1' op e2')
             (Doub, Doub) -> return (Bool, A.ERel Doub e1' op e2')
-            _                    -> throwError $ "Type mismatch: " ++ printTree e1 ++ " " ++ printTree op ++ " " ++ printTree e2 ++ ". Cannot apply '" ++ printTree op ++ "' to " ++ printTree t1 ++ " and " ++ printTree t2
+            _                    -> throwError $ fmt ["Type mismatch:", pt e1, pt op, pt e2, ". Cannot apply", pt op, "to", pt t1, "and", pt t2]
 
     EAnd e1 e2 -> do
         (t1, e1') <- inferExp e1
         (t2, e2') <- inferExp e2
         case (t1, t2) of
             (Bool, Bool) -> return (Bool, A.EAnd e1' e2')
-            _                    -> throwError $ "Type mismatch: " ++ printTree e1 ++ " " ++ printTree e2 ++ ". Cannot apply '&&' to " ++ printTree t1 ++ " and " ++ printTree t2
+            _                    -> throwError $ fmt ["Type mismatch:", pt e1, "&&", pt e2, ". Cannot apply && to", pt t1, "and", pt t2]
 
     EOr e1 e2 -> do
         (t1, e1') <- inferExp e1
         (t2, e2') <- inferExp e2
         case (t1, t2) of
             (Bool, Bool) -> return (Bool, A.EOr e1' e2')
-            _                    -> throwError $ "Type mismatch: " ++ printTree e1 ++ " " ++ printTree e2 ++ ". Cannot apply '||' to " ++ printTree t1 ++ " and " ++ printTree t2
+            _                    -> throwError $ fmt ["Type mismatch:", pt e1, "||", pt e2, ". Cannot apply || to", pt t1, "and", pt t2]
 
 
-checkExp :: Expr -> Type -> Check A.Expr
-checkExp e t = do
+checkExpr :: Expr -> Type -> Check A.Expr
+checkExpr e t = do
     (t', e') <- inferExp e
-    unless (t == t') $ throwError $ "Type mismatch: " ++ printTree e ++ ". Expected " ++ printTree t ++ ", got " ++ printTree t'
+    unless (t == t') $ throwError $ fmt ["Type mismatch:", pt e, ". Expected", pt t, ", got", pt t']
     return e'
 
 lookupVar :: Ident -> Check Type
 lookupVar x = gets stCxt >>= (\case
-    [] -> throwError $ "unbound variable: " ++ printTree x
+    [] -> throwError $ "unbound variable: " ++ pt x
     (t:_) -> return t) . mapMaybe (Map.lookup x) . toList
 
 newVar :: Ident -> Type -> Check ()
 newVar x t = case t of
-    Void -> throwError $ "variable void type not allowed: " ++ printTree x
-    Str -> throwError $ "variable string type not allowed: " ++ printTree x
+    Void -> throwError $ "variable of type void not allowed: " ++ pt x
+    Str -> throwError $ "variable of type string not allowed: " ++ pt x
     _ -> do
         sret <- gets stRet
         b :| bs <- gets stCxt
         let (found, b') = Map.insertLookupWithKey (\ _ t _ -> t) x t b
-        unless (isNothing found) $ throwError $ "variable already defined: " ++ printTree x
+        unless (isNothing found) $ throwError $ "variable already defined: " ++ pt x
         put $ St (b' :| bs) sret
